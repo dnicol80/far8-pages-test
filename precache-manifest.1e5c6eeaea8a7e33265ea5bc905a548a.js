93fc3e086f01e1cc32591992a8b6b3fa3e16496f
Daniel Nicolaevsky <dnicolaevsky@claro.com.ar>
2020-07-30 21:55:56 -0300

    Updates

---
+++ precache-manifest.1e5c6eeaea8a7e33265ea5bc905a548a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0e57a787fdc67c0ae4b2e80f5b9e66f3",
+    "url": "/far8-pages-test/index.html"
+  },
+  {
+    "revision": "9578289a9a244e0f7222",
+    "url": "/far8-pages-test/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "5ec153c002b11b954c87",
+    "url": "/far8-pages-test/static/js/2.bc70d171.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/far8-pages-test/static/js/2.bc70d171.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9578289a9a244e0f7222",
+    "url": "/far8-pages-test/static/js/main.d47cb98d.chunk.js"
+  },
+  {
+    "revision": "c2610dccff636d94d82a",
+    "url": "/far8-pages-test/static/js/runtime-main.cadcb221.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/far8-pages-test/static/media/logo.5d5d9eef.svg"
+  }
+]);
